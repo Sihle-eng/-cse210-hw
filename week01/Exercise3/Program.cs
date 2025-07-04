153b39731687c1c66460d030f9320b5a376fb45f
Sihle-eng <smchobokazi@byupathway.edu>
2025-07-04 09:03:59 +0200

    Completed loop assigments

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int number;
+        int guessNumber;
+    
+        do
+        {
+            Random randomGenerator = new Random();
+            number = randomGenerator.Next(1, 100);
+    
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+    
+            if (guessNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }while (guessNumber != number);
+        
     }
 }
