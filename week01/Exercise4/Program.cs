98e0f4cd55d31e5abebfb5caa2ea9cd5290d6677
Sihle-eng <smchobokazi@byupathway.edu>
2025-07-04 13:13:51 +0200

    Completed lists and generics assigment

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,33 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number;
+        List <int> numbers = new List <int> ();
+        int total = 0;
+        int len;
+        int real;
+        int average;
+        int great = 0;
+
+        do 
+        {
+            Console.Write("Enter number: ");
+            string entered = Console.ReadLine();
+            number = int.Parse(entered);
+            numbers.Add(number);
+            total += number;
+            len = numbers.Count;
+        }while (number != 0);
+
+        Console.WriteLine($"The sum is {total}");
+        real = len - 1;
+        average = total / real;
+        Console.WriteLine($"The average is {average}");
+        
     }
 }
