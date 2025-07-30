756de45a71b849f03451632f360e95fbdbcd1698
Sihle-eng <smchobokazi@byupathway.edu>
2025-07-30 12:05:20 +0200

    Completed learning activities

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assigment = new Assignment("James Bond", "Fuctions");
+        // Console.WriteLine(assigment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("James Bond", "Functions", "Section 5.3", "1,2,3");
+        // Console.WriteLine(mathAssignment.GetSummary());
+        // Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("James Bond", "Fuctions", "The Fuctions of C#");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
+
     }
-}
+} 
